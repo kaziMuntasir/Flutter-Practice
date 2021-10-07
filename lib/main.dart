cfbae1ffc2dd22a125e10906c612849678cde5f1
kaziMuntasir <k,shiam9991@gmail.com>
2021-10-07 12:16:10 +0600

    Practice

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home:JustTry() ,
+  ));
+}
+class JustTry extends StatefulWidget {
+  const JustTry({Key key}) : super(key: key);
+
+  @override
+  _JustTryState createState() => _JustTryState();
+}
+
+class _JustTryState extends State<JustTry> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
